59372262d0d6e3d25ce9f450b6244d3c1fc91b46
Scott Atkinson <scott.atkin79@gmail.com>
2023-10-07 10:20:36 +0000

    added event listeners with keyUp splicing

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ window.addEventListener('load', function () {
     canvas.height = 720;
 
     class InputHandler {
-
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight') &&
+                    this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            });
+            window.addEventListener('keyup', e => {
+                if (e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            });
+        }
     }
 
     class Player {
@@ -27,6 +48,8 @@ window.addEventListener('load', function () {
     function displayStatusText() {
 
     }
+
+    const input = new InputHandler();
 
     function animate() {
 
