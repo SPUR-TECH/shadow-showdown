7531cfa02995c5beb9ec54af190c792d9122bf5c
Scott Atkinson <scott.atkin79@gmail.com>
2023-10-08 14:39:57 +0000

    added backround layers for future parallax effect

--- script.js
+++ script.js
@@ -138,6 +138,21 @@ window.addEventListener('load', function () {
         }
     }
 
+    // const backgroundLayer1 = new Image();
+    // backgroundLayer1.src = './layer-1.png';
+
+    // const backgroundLayer2 = new Image();
+    // backgroundLayer2.src = './layer-2.png';
+
+    // const backgroundLayer3 = new Image();
+    // backgroundLayer3.src = './layer-3.png';
+
+    // const backgroundLayer4 = new Image();
+    // backgroundLayer4.src = './layer-4.png';
+
+    // const backgroundLayer5 = new Image();
+    // backgroundLayer5.src = './layer-5.png';
+
     class Background {
         constructor(gameWidth, gameHeight) {
             this.gameWidth = gameWidth;
