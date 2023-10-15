7b14449600efd3ead4931c58dd7c840620ca05bd
Scott Atkinson <scott.atkin79@gmail.com>
2023-10-15 08:45:10 +0000

    changed canvas height

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext('2d');
     canvas.width = 1600;
-    canvas.height = 720;
+    canvas.height = 710;
     let enemies = [];
     let score = 0;
     let gameOver = false;
